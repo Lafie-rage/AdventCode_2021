784ab5b789b059dc514f489dcee623cb792efaba
lafie-rage <corentin.destrez62@gmail.com>
2021-12-07 11:16:14 +0100

    Day 2 done

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+fun day2() {
+    println("DAY 2 :")
+    val position = Position()
+    File("./src/main/resources/day2.txt").forEachLine { line ->
+        val value  = line.substringAfter(" ").toInt()
+        when {
+            line.contains("forward") -> {
+                position.forward(value)
+            }
+            line.contains("up") -> {
+                position.up(value)
+            }
+            line.contains("down") -> {
+                position.down(value)
+            }
+            else -> {
+                println("Unknown direction : $line")
+            }
+        }
+    }
+    println("New $position")
+    println("Answer is : ${position.horizontal * position.depth}\n\n")
+}
---
+++ src/main/kotlin/Position.kt
@@ -0,0 +1,35 @@
+class Position {
+    var horizontal = 0
+        private set
+    var depth = 0
+        private set
+    var aim = 0
+        private set
+
+    fun up(value: Int) {
+        // Part 1
+        // depth -= value
+        // Part 2
+        aim -= value
+    }
+
+    fun down(value: Int) {
+        // Part 1
+        // depth += value
+        // Part 2
+        aim += value
+    }
+
+    fun forward(value: Int) {
+        horizontal += value
+        // Part 2
+        depth += aim * value
+    }
+
+    override fun toString(): String {
+        return "Position: {\n" +
+                "\thorizontal : $horizontal, \n" +
+                "\tdepth : $depth\n" +
+                "}"
+    }
+}
